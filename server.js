577775db399573f90d7a11edbf3d7c8d688c3d5b
Daniel Doyle <dbdoyle182@gmail.com>
2018-05-23 22:31:52 -0400

    setting up servers

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/app/public'));
+
+require('./app/routes/htmlRoutes.js')(app)
+
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:3000")
+})
